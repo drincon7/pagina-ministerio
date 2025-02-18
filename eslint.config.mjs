2414cf6cefe11b17bca73c5d2b9d5f873c541c9e
drincon7 <spock412@gmail.com>
2025-02-18 16:43:57 -0500

    eslint y prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginNext from "@next/eslint-plugin-next";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      js: pluginJs,
+      react: pluginReact,
+      next: pluginNext,
+      "@typescript-eslint": tseslint.plugin,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+      // Reglas de Prettier
+      "prettier/prettier": "error", // Marca como error los problemas de formato
+      "react/react-in-jsx-scope": "off",
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+    },
+  },
+  prettier, // Desactiva reglas en conflicto con Prettier
 ];
-
-export default eslintConfig;
